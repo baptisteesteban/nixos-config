0462c43ef153bc139902c60e9a932e5ae9530e12
Baptiste Esteban <baptiste.esteban@epita.fr>
2026-04-11 11:11:52 +0200

    Install nixos on decarabia

--- hosts/decarabia/default.nix
+++ hosts/decarabia/default.nix
@@ -4,6 +4,9 @@
   ];
 
   boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # System level modules
