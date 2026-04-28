365ce9b57e7f9f4827d37e5166d6f5c34504406b
Baptiste Esteban <baptiste.esteban@epita.fr>
2026-04-28 10:19:50 +0200

    fish: disable greeting

--- modules/home/shell/fish.nix
+++ modules/home/shell/fish.nix
@@ -8,6 +8,11 @@ in {
   options.my.home.fish.enable = lib.mkEnableOption "Enable Fish shell";
 
   config = lib.mkIf cfg.enable {
-    programs.fish.enable = true;
+    programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting
+      '';
+    };
   };
 }
