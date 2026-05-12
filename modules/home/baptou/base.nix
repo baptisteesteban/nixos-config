432ee76205bfa3203a5bcc48ae353ca16c9e888b
Baptiste Esteban <baptiste.esteban@epita.fr>
2026-05-12 11:46:24 +0200

    Add zip utilities

--- modules/home/baptou/base.nix
+++ modules/home/baptou/base.nix
@@ -1,7 +1,12 @@
 {config, ...}: {
-  flake.modules.homeManager.baptou-base = {
+  flake.modules.homeManager.baptou-base = {pkgs, ...}: {
     home.username = "baptou";
     home.homeDirectory = "/home/baptou";
+
+    home.packages = with pkgs; [
+      zip # Zip compression
+      unzip # Zip decompression
+    ];
 
     imports = [
       config.flake.modules.homeManager.bat
